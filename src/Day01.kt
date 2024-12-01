ba0b669a1a947ce154f97d3ccb921a38afb27285
Jacob Botuck <jbotuck@gmail.com>
2024-12-01 11:39:08 -0600

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,7 @@
 import kotlin.math.abs
 
 fun main() {
+    //parse
     val (left, right) = readInput("Day01").map { line ->
         line.split(" ")
             .filter { it.isNotBlank() }
@@ -8,8 +9,13 @@ fun main() {
             .let { it.first() to it.last() }
     }.unzip()
 
-    left.sorted().zip(right.sorted()).sumOf { (a, b) -> abs(a - b) }.let { println("PART 1: $it") }
+    //solve 1
+    left.sorted().zip(right.sorted()).sumOf { (a, b) -> abs(a - b) }
+        .let { println("PART 1: $it") }
+
+    //solve 2
     val histogram = right.groupBy { it }.mapValues { it.value.size }
-    println("PART 2: ${left.sumOf { histogram.getOrDefault(it, 0) * it }}")
+    left.sumOf { histogram.getOrDefault(it, 0) * it }
+        .let { println("PART 2: $it") }
 
 }
