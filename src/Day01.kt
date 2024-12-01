84d5add28b677dc4a667afdb633a80601c07c1a2
Jacob Botuck <jbotuck@gmail.com>
2024-12-01 11:23:12 -0600

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,15 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val (left, right) = readInput("Day01").map { line ->
+        line.split(" ")
+            .filter { it.isNotBlank() }
+            .map { it.toInt() }
+            .let { it.first() to it.last() }
+    }.unzip()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    left.sorted().zip(right.sorted()).sumOf { (a, b) -> abs(a - b) }.let { println("PART 1: $it") }
+    val histogram = right.groupBy { it }.mapValues { it.value.size }
+    println("PART 2: ${left.sumOf { histogram.getOrDefault(it, 0) * it }}")
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
