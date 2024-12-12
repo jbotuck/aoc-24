023370ae516d09f517aae59ab515f8a0c519d5e2
Jacob Botuck <jbotuck@gmail.com>
2024-12-11 21:08:52 -0600

    day 9 part 1 rewrite works

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,66 +1,49 @@
-import java.util.*
-import kotlin.collections.ArrayDeque
-import kotlin.collections.first
-import kotlin.collections.isNotEmpty
-import kotlin.collections.mutableListOf
-
 fun main() {
     //val compactDiskMap = "2333133121414131402"
     val compactDiskMap = readInput("Day09").first()
-    val (fileList, fileMap, freeSpace) = run {
-        val (files, freeSpace) = sequence {
-            var id = 0
-            var fileMode = true
-            var index = 0
+    val expandedMap = sequence {
+        var id = 0
+        var fileMode = true
 
-            @Suppress("NAME_SHADOWING")
-            val compactDiskMap = ArrayDeque(compactDiskMap.map { it.digitToInt() })
-            while (compactDiskMap.isNotEmpty()) {
-                val blockSize = compactDiskMap.removeFirst()
-                if (blockSize != 0)
-                    yield(Pair(if (fileMode) id else -1, index until index + blockSize))
-                if (fileMode) id++
-                fileMode = !fileMode
-                index += blockSize
+        @Suppress("NAME_SHADOWING")
+        val compactDiskMap = ArrayDeque(compactDiskMap.map { it.digitToInt() })
+        while (compactDiskMap.isNotEmpty()) {
+            repeat(compactDiskMap.removeFirst()) {
+                yield(if (fileMode) id else -1)
             }
-        }.partition { it.first >= 0 }
-        val fileMap = files.groupBy({ it.first }, { it.second }).toMutableMap()
-        val queue = PriorityQueue<IntRange>(
-            Comparator.comparingInt { it.first }
-        ).apply {
-            addAll(freeSpace.map { it.second })
+            if (fileMode) id++
+            fileMode = !fileMode
         }
-        Triple(files.map { it.first }, fileMap, queue)
+    }.toMutableList()
+    var indexOfFreeSpace = expandedMap.indexOf(-1)
+    var indexOfLastUsedSpace = expandedMap.indexOfLast { it != -1 }
+    while (indexOfLastUsedSpace > indexOfFreeSpace) {
+        expandedMap[indexOfFreeSpace] = expandedMap[indexOfLastUsedSpace]
+        expandedMap[indexOfLastUsedSpace] = -1
+        indexOfLastUsedSpace = expandedMap.indexOfLastUsedSpace(indexOfLastUsedSpace) ?: break
+        indexOfFreeSpace = expandedMap.indexOfFreeSpace(indexOfFreeSpace.inc()) ?: break
     }
-
-    for (file in fileList.reversed()) {
-        var fileBlock = fileMap[file]!!.first()
-        val newFileBlocks = mutableListOf<IntRange>()
-        while (!fileBlock.isEmpty()) {
-            val freeBlock = freeSpace.poll()
-            if (freeBlock == null || freeBlock.first > fileBlock.first) {
-                newFileBlocks.add(fileBlock)
-                break
-            }
-            val fileBlockSize = fileBlock.size()
-            newFileBlocks.add(freeBlock.takeFirst(fileBlockSize))
-            freeBlock.dropFirst(fileBlockSize).takeUnless { it.isEmpty() }?.let { freeSpace.add(it) }
-            val freeBlockSize = freeBlock.size()
-            freeSpace.add(fileBlock.takeLast(freeBlockSize))
-            fileBlock = fileBlock.dropLast(freeBlockSize)
-        }
-        fileMap[file] = newFileBlocks
-    }
-
-    fileMap.map { (id, blocks) ->
-        blocks.sumOf { block -> block.sumOf { it * id }.toLong() }
-    }.sum().also { println(it) }
-
+    println(expandedMap)
+    expandedMap.withIndex()
+        .filter { it.value >= 1 }
+        .sumOf { (position, id) -> position * id.toLong() }
+        .also { println(it) }
 }
 
-private fun IntRange.size() = last.inc().minus(first).coerceAtLeast(0)
-private fun IntRange.takeFirst(n: Int): IntRange = start until start.plus(n).coerceAtMost(endInclusive.inc())
-private fun IntRange.takeLast(n: Int): IntRange = endInclusive.inc().minus(n).coerceAtLeast(start)..endInclusive
-private fun IntRange.dropFirst(n: Int): IntRange = start.plus(n)..endInclusive
-private fun IntRange.dropLast(n: Int): IntRange = start..endInclusive.minus(n)
+private fun MutableList<Int>.indexOfFreeSpace(start: Int): Int? {
+    var current = start
+    while (current in indices) {
+        if (get(current) == -1) return current
+        current++
+    }
+    return null
+}
 
+private fun MutableList<Int>.indexOfLastUsedSpace(end: Int): Int? {
+    var current = end.dec()
+    while (current in indices) {
+        if (get(current) != -1) return current
+        current--
+    }
+    return null
+}
